d17c415c8a99b2aa14ca3c2df062d5a8b66c914d
Raees Azam Shaikh <shaikhraishazam@gmail.com>
2024-04-28 16:34:15 +0530

    this test file we check coding work properly or not

--- tests.py
+++ tests.py
@@ -2,13 +2,20 @@ from source.loggers import logging
 from source.exceptions import CustomException
 import os ,sys
 from source.components.data_ingestion import DataIngestion,DataIngestionConfig
+from source.components.data_transformation import DataTransformation,DataIngestionConfig
 
 
 def main():
     logging.info('Just Checking artifacts Created Or not')
     try:
         di = DataIngestion()
-        di.initiate_data_ingestion()
+        raw_path,train_path,test_path = di.initiate_data_ingestion()
+
+        #creating object of DataTransformation class
+        dt = DataTransformation()
+        train_array,test_array,preprocessor_filepath = dt.initiate_data_transformation(raw_path,train_path,test_path)
+
+        logging.info('train array will be\n%s',train_array)
 
     except Exception as e:
         raise CustomException(e,sys)
