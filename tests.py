b2bcacc02876453143c944eeab5d85ee643fdd12
Raees Azam Shaikh <shaikhraishazam@gmail.com>
2024-05-05 00:08:30 +0530

    this file we used for testing the model working properly or not

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ from source.exceptions import CustomException
 import os ,sys
 from source.components.data_ingestion import DataIngestion,DataIngestionConfig
 from source.components.data_transformation import DataTransformation,DataIngestionConfig
+from source.components.model_trainer import ModelTrainer,ModelTrainerConfig
 
 
 def main():
@@ -14,8 +15,13 @@ def main():
         #creating object of DataTransformation class
         dt = DataTransformation()
         train_array,test_array,preprocessor_filepath = dt.initiate_data_transformation(raw_path,train_path,test_path)
-
         logging.info('train array will be\n%s',train_array)
+
+        #creating an object of ModelTrainer class
+        mt = ModelTrainer()
+        score = mt.initiate_training(train_array,test_array)
+
+        logging.info('The Best Model score is \n%s',score)
 
     except Exception as e:
         raise CustomException(e,sys)
